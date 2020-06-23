c5770b9a7ccaca06a61650b52087a69a128848d8
ghumphr@ghumphr-us-la <graeme_humphrey@trimble.com>
2020-06-23 11:55:02 -0600

    added feed only project

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=
-export POSTGRES_PASSWORD=
-export POSTGRES_HOST=
-export POSTGRES_DB=
-export AWS_BUCKET=
-export AWS_REGION=
-export AWS_PROFILE=
-export JWT_SECRET=
+export POSTGRES_USERNAME=rdsfree
+export POSTGRES_PASSWORD=SMEGHEAD17559
+export POSTGRES_HOST=database-1.c9bf8go3pwkz.us-west-2.rds.amazonaws.com
+export POSTGRES_DB=database-1
+export AWS_BUCKET=microservices-project
+export AWS_REGION=us-west-2
+export AWS_PROFILE=microservices-project-at-778243431115
+export JWT_SECRET=SMEGHEAD17559
 export URL=http://localhost:8100
